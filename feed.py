c4a3d9b863abdb478422dbb8964123a03ef6b269
Isaac A. White <whitis01@gmail.com>
2021-02-24 19:01:00 -0600

    Working to get feed to be more robust.

--- feed.py
+++ feed.py
@@ -6,7 +6,13 @@ print("Hello, " + x)
 
 # For now we will hard code an RSS feed into the Feed parser.
 NewsFeed = feedparser.parse(x)
-entry = NewsFeed.entries[0]
+# entry = NewsFeed.entries[0]
 
-print(entry.values())
+print(len(NewsFeed.entries))
+
+for post in NewsFeed.entries:
+    print(post.title + " : " + post.link)
+
+#print(NewsFeed.values())
+print(NewsFeed.keys())
 
